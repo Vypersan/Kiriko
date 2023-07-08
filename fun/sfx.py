49af49f937fd399486ff69521e63cf4fa4cdcf84
Senpai_Desi <72793345+SenpaiDesi@users.noreply.github.com>
2023-07-08 04:02:25 +0200

    Added 6 more sounds

--- fun/sfx.py
+++ fun/sfx.py
@@ -32,6 +32,12 @@ class sfx(commands.Cog):
         Choice(name="The weak are food for the strong", value="theweak"),
         Choice(name="That is fox power", value="foxpower"),
         Choice(name="So long bestie", value="bestie"),
+        Choice(name="Some of the old, some of the new", value="someoldsomenew"),
+        Choice(name="Push a rock", value="rock"),
+        Choice(name="Lonely in here", value="lonely"),
+        Choice(name="Keep calm, Keep sly", value="sly"),
+        Choice(name="Hey Queen", value="queen"),
+        Choice(name="Laughing", value="laugh")
     ])
     async def playsfx(self, interaction : discord.Interaction, line:str):
         channel = interaction.user.voice.channel
@@ -39,7 +45,7 @@ class sfx(commands.Cog):
             vc = await channel.connect()
         except discord.ClientException:
             return await interaction.response.send_message(f"Sorry I am already playing in another channel. That channel being {channel.mention}", ephemeral=True)
-        source= discord.FFmpegPCMAudio(source= f"/root/Kiriko/fun/sfx/{line}.ogg")
+        source= discord.FFmpegPCMAudio(source= f"D:/littlefox/Repos/Kiriko/fun/sfx/{line}.ogg")
         await interaction.response.send_message(f"Okay, Playing in {channel.mention}", ephemeral=True)
         vc.play(source)
         while vc.is_playing():
